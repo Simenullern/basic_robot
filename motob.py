f1c8f1dddd1b106db95183ae1a29ee7ed46fbcbc
Simen Ullern <simenullern@gmail.com>
2016-11-10 11:27:00 +0100

    make motor stop b4 turn

--- motob.py
+++ motob.py
@@ -11,9 +11,11 @@ class Motob():
 
     def operationalize(self):
         if self.value[0] == "left":
+            self.motor.stop()
             self.motor.turn_left(self.value[1])
             self.motor.forward(0.5)
         elif self.value[0] == "right":
+            self.motor.stop()
             self.motor.turn_right(self.value[1])
             self.motor.forward(0.5)
         elif self.value[0] == "drive":
