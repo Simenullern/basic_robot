e3c488e8a5a319932cf9febb83d4463c7f65777f
kjetkv <kjetil.kvrnum@gmail.com>
2016-11-10 11:49:49 +0100

    fjerna sensors2

--- bbcon.py
+++ bbcon.py
@@ -61,7 +61,7 @@ def main():
     #sensob2 = Reflect_Sensob(sensorReflect)
     #sensob3 = Camera_Sensob(sensorCam)
 
-    bbcon = BBCON([sensob0, sensob1, sensob2, sensob3])
+    bbcon = BBCON([sensob0, sensob1]) #sensob2, sensob3
 
     drive = Move_straight_ahead(bbcon)
     avoid_shit = Avoid_front_collision(bbcon,[sensob0, sensob1])
