0ad6101622d33098eab9ca2eb894f4cb9b753fc5
kjetkv <kjetil.kvrnum@gmail.com>
2016-11-10 11:22:40 +0100

    imports removed

--- bbcon.py
+++ bbcon.py
@@ -1,16 +1,10 @@
 from time import sleep
-import random
-import imager2 as IMR
 from reflectance_sensors import ReflectanceSensors
 from camera import Camera
-from motors import Motors
 from motob import *
-from ultrasonic import Ultrasonic
-from irproximity_sensor import  IRProximitySensor
 from zumo_button import ZumoButton
 from arbitrator import Arbitrator
 from sensob import *
-import time
 from behavior import *
 
 class BBCON():
